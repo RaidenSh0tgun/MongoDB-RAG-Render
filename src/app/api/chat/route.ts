0076ebb91295e9a8d84a19fa64b0c8c051772225
RaidenSh0tgun <153772739+RaidenSh0tgun@users.noreply.github.com>
2024-12-05 23:55:02 -0500

    Update route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -31,7 +31,7 @@ export async function POST(req: Request) {
             outputParser: new StringOutputParser(),
         });
         ragChain.invoke({
-            question:: question,
+            question: question,
             
         })
 
