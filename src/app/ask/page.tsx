29c3ce56d071933293a08ff660ddd0644d816ee4
RaidenSh0tgun <153772739+RaidenSh0tgun@users.noreply.github.com>
2024-12-06 18:22:44 -0500

    Update page.tsx

--- src/app/ask/page.tsx
+++ src/app/ask/page.tsx
@@ -35,9 +35,9 @@ export default function Home() {
         </>
         <div className="pr-4 messages">
           {messages.map(m => (
-            <div key={m.id} className="flex gap-3 my-4 text-gray-600 text-sm flex-1">
+            <div key={m.id} className="flex gap-3 my-4 text-gray-600 text-lg flex-1">
               <span className="relative flex shrink-0 overflow-hidden rounded-full w-6 h-6" 
-                    style={{ margin: '30px', marginTop: '0px' }}>
+                    style={{ margin: '60px', marginTop: '0px' }}>
                 <div className="rounded-full bg-gray-100 border p-1">
                   {m.role === 'user' ? (
                     <img src="/Fairy.png" />
@@ -60,13 +60,13 @@ export default function Home() {
             <input
               value={input}
               onChange={handleInputChange}
-              className="flex h-20 w-full rounded-md border border-[#e5e7eb] px-4 py-2 text-sm placeholder-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#9ca3af] disabled:cursor-not-allowed disabled:opacity-50 text-[#030712] focus-visible:ring-offset-2"
+              className="flex h-20 w-full rounded-md border border-[#e5e7eb] px-4 py-2 text-base placeholder-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#9ca3af] disabled:cursor-not-allowed disabled:opacity-50 text-[#030712] focus-visible:ring-offset-2"
               placeholder="What do you want to know about Tong Chen?"
               style={{ fontSize: '18px' }}
             />
             <button
               type="submit"
-              className="inline-flex items-center justify-center rounded-md text-sm font-medium text-[#f9fafb] disabled:pointer-events-none disabled:opacity-50 bg-black hover:bg-[#111827E6] h-10 px-4 py-2"
+              className="inline-flex items-center justify-center rounded-md text-base font-medium text-[#f9fafb] disabled:pointer-events-none disabled:opacity-50 bg-black hover:bg-[#111827E6] h-10 px-4 py-2"
               style={{ fontSize: '18px' }}
               >
               Send
