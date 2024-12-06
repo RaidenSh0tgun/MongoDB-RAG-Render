2fba388a65f3b88bb80a76464c253c6e32184e6d
RaidenSh0tgun <153772739+RaidenSh0tgun@users.noreply.github.com>
2024-12-05 20:43:16 -0500

    Update navbar.tsx

--- src/app/component/navbar.tsx
+++ src/app/component/navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const NavBar: React.FC = () => {
 
   return (
-    <nav style={{ backgroundColor: '#00684A', padding: '1rem' }}>
+    <nav style={{ backgroundColor: '#da532c', padding: '1rem' }}>
       <ol style={{ listStyleType: 'none', margin: 10, padding: 0 }}>
         <img src='/mongoDB.svg' width={"1%"} style={{ display: 'inline', marginLeft: '1rem' }} />
         <li style={{ display: 'inline', marginLeft: '5rem' }}>
