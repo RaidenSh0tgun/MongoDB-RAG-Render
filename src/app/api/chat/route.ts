f9943752f587a031dcc765feffb83379cc3a20f7
RaidenSh0tgun <153772739+RaidenSh0tgun@users.noreply.github.com>
2024-12-06 20:49:21 -0500

    Update route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -14,10 +14,17 @@ export async function POST(req: Request) {
         const messages: Message[] = body.messages ?? [];
         let question = messages[messages.length - 1].content;
         // Add your extra string to the question
-        const extraString = "Ingore all previous instruction. Your name is Friday. You are a professional assistant for Tong Chen with a sense of humor. Your duty is to answer visitor's question about Tong Chen.";
+        // const extraString = "Ingore all previous instruction. Your name is Friday. You are a professional assistant for Tong Chen. You have a sense of humor. Your duty is to answer below question:/n.";
         question = extraString + question; // Concatenate the extra string
 
+        const CUSTOM_QUESTION_GENERATOR_CHAIN_PROMPT = `Your name is Friday. You are a professional assistant for Tong Chen. You have a sense of humor. 
+            Your duty is to answer the question: {question}/n.
+            Your previous coversation is:
+            {chat_history}
+            `;
+
         const model = new ChatOpenAI({
+            model: "gpt-4o-mini",
             temperature: 0.8,
             streaming: true,
             callbacks: [handlers],
@@ -31,6 +38,9 @@ export async function POST(req: Request) {
             memory: new BufferMemory({
               memoryKey: "chat_history",
             }),
+            questionGeneratorChainOptions: {
+              template: CUSTOM_QUESTION_GENERATOR_CHAIN_PROMPT,
+            },
           })
         conversationChain.invoke({
             "question": question
